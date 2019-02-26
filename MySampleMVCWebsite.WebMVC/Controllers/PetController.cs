0c823974117a7781bff83232b9228a32771d9a71
Debbie <debbieforbeck@gmail.com>
2019-02-26 17:05:41 -0500

    Missing Delete and Details

---
+++ MySampleMVCWebsite.WebMVC/Controllers/PetController.cs
@@ -0,0 +1,71 @@
+﻿using MySampleMVCWebsite.WebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using static MySampleMVCWebsite.WebMVC.Models.Pet;
+
+namespace MySampleMVCWebsite.WebMVC.Controllers
+{
+    public class PetController : Controller
+    {
+        private PetDBContext db = new PetDBContext();
+
+        // GET: Pet
+        public ActionResult Index()
+        {
+            return View(db.Pets.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Pet/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PetID,Name,AnimalType")] Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pets.Add(pet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pet);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Pet pet = db.Pets.Find(id);
+
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PetID, Name, AnimalType")]Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pet).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pet);
+        }
+
+    }
+}
---
+++ MySampleMVCWebsite.WebMVC/Models/Pet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MySampleMVCWebsite.WebMVC.Models
+{
+    public class Pet
+    {
+        public int PetID { get; set; }
+        public string Name { get; set; }
+        public string AnimalType { get; set; }
+
+    public class PetDBContext : DbContext
+        {
+            public DbSet<Pet>Pets { get; set; }
+        }
+
+    }
+}
