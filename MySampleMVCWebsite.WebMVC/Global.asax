6039f1c8c70ffcb7ba75b1cd99888a41ccdd9618
Debbie <debbieforbeck@gmail.com>
2019-02-26 14:34:23 -0500

    Add project files.

---
+++ MySampleMVCWebsite.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MySampleMVCWebsite.WebMVC.MvcApplication" Language="C#" %>
---
+++ MySampleMVCWebsite.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySampleMVCWebsite.WebMVC.Startup))]
+namespace MySampleMVCWebsite.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
